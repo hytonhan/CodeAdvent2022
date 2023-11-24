983655cf924f056d234a52182c528224269a7257
hytonhan <hannuhytonen@gmail.com>
2023-11-24 23:17:27 +0200

    day 13

--- CodeAdvent2022/Day13.cs
+++ CodeAdvent2022/Day13.cs
@@ -1,4 +1,5 @@
-﻿using SimpleInjector;
+﻿using Newtonsoft.Json;
+using SimpleInjector;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,38 +16,89 @@ namespace CodeAdvent2022
 
         public void Run()
         {
-            string[] lines = File.ReadAllLines(_testInputFilename);
+            string[] lines = File.ReadAllLines(_inputFilename);
+
+            List<int> indices = new();
+            List<int> values = new();
+
+            List<List<object>> packets = new();
 
             for (int i = 0; i < lines.Length; i += 3)
             {
+                int index = i / 3 + 1;
                 var first = lines[i];
                 var second = lines[i + 1];
 
                 var packet1 = ParseList(new Queue<char>(first.ToCharArray()));
                 var packet2 = ParseList(new Queue<char>(second.ToCharArray()));
 
-                Compare(packet1, packet2);
+                packets.Add(packet1);
+                packets.Add(packet2);
+
+                int result = Compare(packet1, packet2);
+
+                string foo = result == -1 ? "Not in order" : "in order";
+
+                if (result >= 0)
+                {
+                    indices.Add(index);
+                    values.Add(result);
+                }
+                //Console.WriteLine($"Packet index: {index}. Result: {foo}");
             }
+
+            var sum = indices.Sum();
+            Console.WriteLine($"Sum of indices of ordered packets: {sum}");
+
+            List<object> divider1 = new List<object>()
+            {
+                new List<object>()
+                {
+                    2
+                }
+            };
+            List<object> divider2 = new List<object>()
+            {
+                new List<object>()
+                {
+                    6
+                }
+            };
+
+            packets.Add(divider1);
+            packets.Add(divider2);
+            packets.Sort(Compare);
+            packets.Reverse();
+
+            var index1 = packets.IndexOf(divider1) + 1;
+            var index2 = packets.IndexOf(divider2) + 1;
+
+            Console.WriteLine($"Product of divider indices: {index1 * index2}");
         }
 
         private int Compare(List<object> packet1, List<object> packet2)
         {
-            for (int i = 0; i <= packet1.Count; i++)
+            if (packet1.Count == 0 && packet2.Count > 0)
             {
-                if (i >= packet1.Count)
+                return 1;
+            }
+            for (int i = 0; i < packet1.Count; i++)
+            {
+                if (i >= packet2.Count)
                 {
                     return -1;
                 }
                 var item1 = packet1[i];
                 var item2 = packet2[i];
 
-                if(item1.GetType() == typeof(List<object>) && item2.GetType() == typeof(List<object>))
+                if (item1.GetType() == typeof(List<object>) && item2.GetType() == typeof(List<object>))
                 {
                     var comp = Compare((List<object>)item1, (List<object>)item2);
-                    if(comp != 0)
+                    if (comp != 0)
                     {
                         return comp;
                     }
+                    continue;
                 }
 
                 if (item1.GetType() == typeof(int) && item2.GetType() == typeof(int))
@@ -59,7 +111,36 @@ namespace CodeAdvent2022
                     {
                         return -1;
                     }
+                    continue;
                 }
+                if (item1.GetType() == typeof(int))
+                {
+                    var foo = new List<object>()
+                    {
+                        (int)item1
+                    };
+                    var comp = Compare((List<object>)foo, (List<object>)item2);
+                    if (comp != 0)
+                    {
+                        return comp;
+                    }
+                }
+                else if (item2.GetType() == typeof(int))
+                {
+                    var foo = new List<object>()
+                    {
+                        (int)item2
+                    };
+                    var comp = Compare((List<object>)item1, (List<object>)foo);
+                    if (comp != 0)
+                    {
+                        return comp;
+                    }
+                }
+            }
+            if (packet1.Count < packet2.Count)
+            {
+                return 1;
             }
             return 0;
 
@@ -89,7 +170,13 @@ namespace CodeAdvent2022
                 }
                 else
                 {
-                    temp.Add(char.GetNumericValue(input.Dequeue()));
+                    int number = (int)char.GetNumericValue(input.Dequeue());
+                    if (input.Peek() == '0')
+                    {
+                        number *= 10;
+                        input.Dequeue();
+                    }
+                    temp.Add(number);
                 }
             }
 
